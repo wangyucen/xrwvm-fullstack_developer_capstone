53edde2b2d70a49a48c33549b84374cca6038392
wangyucen <jane97622@gmail.com>
2024-05-20 10:45:28 -0400

    Dynamic Pages

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,17 @@
 import LoginPanel from "./components/Login/Login"
+import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer"
 import Register from "./components/Register/Register";
+import PostReview from "./components/Dealers/PostReview"
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/register" element={<Register />} />
+      <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
       <Route path="/login" element={<LoginPanel />} />
     </Routes>
   );
